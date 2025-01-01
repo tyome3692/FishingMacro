8f9c4b28c2a50ff568d1274a4292bca486ffdecb
yutsu <yutsu@DESKTOP-J8664UB>
2025-01-01 18:42:55 +0900

    feat: コンソールのアタッチじに機能を追加

--- FIshingMacro/NativeMethods.cs
+++ FIshingMacro/NativeMethods.cs
@@ -74,15 +74,80 @@ namespace FIshingMacro
             internal int x, y;
         }
 
+        #region Console
+
+        [LibraryImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool AllocConsole();
+        [LibraryImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static partial bool FreeConsole();
+
+        [LibraryImport("user32.dll")]
+        private static partial IntPtr GetSystemMenu(IntPtr hWnd, UInt32 bRevert);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        private static partial UInt32 RemoveMenu(IntPtr hMenu, UInt32 nPosition, UInt32 wFlags);
+        private const UInt32 SC_CLOSE = 0x0000F060;
+        private const UInt32 SC_MAXIMIZE = 0xF030;
+        private const UInt32 MF_BYCOMMAND = 0x0;
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        private static partial IntPtr GetStdHandle(int nStdHandle);
+
+        [LibraryImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool GetConsoleMode(IntPtr hConsoleHandle, out uint lpMode);
+
+        [LibraryImport("kernel32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool SetConsoleMode(IntPtr hConsoleHandle, uint dwMode);
+
+        const int STD_INPUT_HANDLE = -10;
+        const uint ENABLE_QUICK_EDIT = 0x0040;
+        #endregion
+
         /// <summary>
         /// 整形済みのコンソールウィンドウをアタッチして最前面に固定する
         /// </summary>
-        public static void AttachConsole()
+        internal static void AttachConsole()
         {
             AllocConsole();
-            Console.Title = "WCConsole";
+
+            Console.Title = "自動釣り機";
             Console.CursorVisible = false;
-            SetWindowPos(GetConsoleWindow(), (IntPtr)(-1), -7, 310, 250, 200, SWP_SHOWWINDOW);
+#pragma warning disable CA2000
+            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+#pragma warning restore CA2000
+            Console.CancelKeyPress += Console_CancelKeyPress;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            IntPtr consoleHandle = GetConsoleWindow();
+            IntPtr closeMenuHandle = GetSystemMenu(consoleHandle, 0);
+
+            if (RemoveMenu(closeMenuHandle, SC_CLOSE, MF_BYCOMMAND) == 0)
+            {
+                Console.WriteLine($"RemoveMenu関数が失敗しました。Error : {Marshal.GetLastWin32Error()}");
+            }
+            if (RemoveMenu(closeMenuHandle, SC_MAXIMIZE, MF_BYCOMMAND) == 0)
+            {
+                Console.WriteLine($"RemoveMenu関数が失敗しました。Error : {Marshal.GetLastWin32Error()}");
+            }
+
+            uint consoleMode = 0;
+            IntPtr inputHandle = GetStdHandle(STD_INPUT_HANDLE);
+            if (!GetConsoleMode(inputHandle, out consoleMode))
+            {
+                Console.WriteLine(Marshal.GetLastWin32Error());
+            }
+            consoleMode &= ~ENABLE_QUICK_EDIT;
+            SetConsoleMode(inputHandle, consoleMode);
+
+            SetWindowPos(GetConsoleWindow(), -1, -7, 310, 250, 200, SWP_SHOWWINDOW);
+        }
+        private static void Console_CancelKeyPress(object? sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
         }
 
         /// <summary>
